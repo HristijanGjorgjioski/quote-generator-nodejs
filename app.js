ac926b97856500a54c4f1e4978726f18ac68b578
HristijanGjorgjioski <hristijangorgioski503@gmail.com>
2021-01-21 10:10:56 +0100

    useless message

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const fs = require('fs');
+let ejs = require('ejs');
+
+const templateString = fs.readFileSync('./views/cards-view.ejs', 'utf-8');
+
+const app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.get('/', (req, res) => {
+  res.send(ejs.render(templateString));
+});
+
+
+
+
+
+app.listen(3000, (req, res) => {
+  console.log("Server running on port 3000..")
+});
